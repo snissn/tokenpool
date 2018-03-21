b877b34a30526a0787ee57ea10d2e80daea5f6cd
mikers <mike@mike.rs>
2018-03-21 03:04:53 -0400

    use _payout wallet

--- sample.account.config.js
+++ sample.account.config.js
@@ -1,6 +1,8 @@
 var account = {
   address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
   privateKey: '00000'
+  address_payouts: '0x0000000000000000000000007137DF705868FD67',
+  privateKey_payouts: '00000'
 }
 
 exports.account = account
