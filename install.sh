eab996f2de29715a1a56450a32e5267fd2ad1f5e
mikers <mike@mike.rs>
2018-04-27 21:33:55 +0000

    install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+set -eux
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs redis-server build-essential
+
+npm install
+npm run webpack
+
+
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum -y
+
+
+
+
+
+echo 
+
+echo 'run 
+
+npm run dev &
+
+node fixed_diff.js &'
+
+
+
+
+
