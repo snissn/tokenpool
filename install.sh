f9078be6397e91eb886f61eb43b337bef0e4d0de
mikers <mike@mike.rs>
2018-04-28 07:49:22 +0000

    install redis

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs redis-server build-essential
 
 npm install
+npm install redis
 npm run webpack
+
 
 
 sudo add-apt-repository -y ppa:ethereum/ethereum
