8d3eae8ebfe0128c8422a0e322fd49146269f834
mikers <mike@mike.rs>
2018-08-28 14:58:32 +0000

    delete redis data

---
+++ scripts/redis/delete_submitted_shares_hash.py
@@ -0,0 +1,19 @@
+#this is the third  script to run, it works effectively and kills old shares 
+
+
+import redis
+import datetime
+import json
+import time
+now = time.time()
+r = redis.StrictRedis(host='10.142.0.4')
+
+key = "submitted_share"
+for hkey in r.hkeys(key):
+  row = json.loads(r.hget(key,hkey))
+  seconds = now - row['time']
+  hours = seconds / 60/60 
+  if hours > 12:
+    r.hdel(key, hkey)
+    #print key,hkey
+
