4a9e44863c3584d282c14f556d27c0f4c93ec64a
mikers <snissn@gmail.com>
2018-09-03 00:13:06 -0400

    update sample configs with merge mining info

--- sample.pool.config.js
+++ sample.pool.config.js
@@ -1,11 +1,12 @@
 
 var poolconfig = {
   minimumShareDifficulty: 105,
-  solutionGasPriceWei: 10,
-  transferGasPriceWei: 6,
+  solutionGasPriceWei: 10, // not used - TODO REMOVE 
+  transferGasPriceWei: 6, // not used - TODO REMOVE 
   poolTokenFee: 15,
   minBalanceForTransfer: 1500000000,
-  populationLimit: 100,
+  mergeMintTokens: ['0x33d99efc0c3cc4f93da6931ec2cccf19ca874b6d', '0x291de53a16b76dfe28551fd3335225f506db8b82'],
+  populationLimit: 100, // not used - TODO REMOVE 
   web3provider: "http://127.0.0.1:8545"
 }
 
