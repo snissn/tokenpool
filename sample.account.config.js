e4ef93af02a4d9c00f85a48f76ed28e89725f8dd
mikers <mike@mike.rs>
2018-10-18 18:23:50 +0000

    use mint helper in account config

--- sample.account.config.js
+++ sample.account.config.js
@@ -3,6 +3,7 @@ var accounts = {
   minting: {
     address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
     privateKey: '00000'
+    helper: '0x50212e78D96A183f415e1235e56E64416d972E93'
   },
 
   payment: {
