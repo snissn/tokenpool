4f9ddb8e096c374ea17aadc16c500376b2065008
mikers <mike@mike.rs>
2018-10-21 16:59:57 +0000

    payout algo

--- multi_payouts/payout.sh
+++ multi_payouts/payout.sh
@@ -1,6 +1,12 @@
 set -eux
 while true; do
   python3 multi_payout.py 3 --pay
-  date
-  sleep 24h;
+  python3 multi_payout.py 3 --pay
+  sleep 6h;
+  python3 multi_payout.py 100 --pay
+  sleep 6h;
+  python3 multi_payout.py 100 --pay
+  sleep 6h;
+  python3 multi_payout.py 100 --pay
+  sleep 6h;
 done
