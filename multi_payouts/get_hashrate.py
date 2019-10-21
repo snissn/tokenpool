aac2f5003b5cd868d0a6b3788afa4f94387cb8ee
mikers <mike@mike.rs>
2019-10-21 13:59:02 +0000

    get miner data in json

--- multi_payouts/get_hashrate.py
+++ multi_payouts/get_hashrate.py
@@ -1,10 +1,34 @@
-import websockets 
+import json
 import websocket
-ws = websocket.WebSocket()
-ws.connect("ws://mike.rs:2095/socket.io/?EIO=3&transport=websocket")
 
-ws.send('42["getHashrateData"]')
-for i in range(10):
+def get_all_hashrates():
+  ws = websocket.WebSocket()
+  ws.connect("ws://mike.rs:2095/socket.io/?EIO=3&transport=websocket")
+
+  ws.send('42["getHashrateData"]')
+  for i in range(10):
+    response = ws.recv()
+    print(response)
+
+
+
+
+
+def get_hashrate(address):
+  ws = websocket.WebSocket()
+  ws.connect("ws://mike.rs:2095/socket.io/?EIO=3&transport=websocket")
+
+  ws.send( '42["getMinerDetails",{"address":"'+address+'"}]')
   response = ws.recv()
-  print(response)
+  response = ws.recv()
+  response = ws.recv()
+  response = json.loads(response[2:])
+  return response[1]
 
+
+
+def main():
+  hashrate = get_hashrate("0x47Ad0177A26b31646C878cC80ba8Fc1494b58c9D")
+  print(hashrate)
+if __name__=="__main__":
+  main()
